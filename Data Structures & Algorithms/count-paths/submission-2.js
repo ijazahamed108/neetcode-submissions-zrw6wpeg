f65f16b4c9c9b812f16a57f1f2a908e05474f5db
Ijaz Ahamed <41547944+ijazahamed108@users.noreply.github.com>
2026-06-30 00:09:24 +0530

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+    let dp = new Array(n).fill(1);
+    for(let row =m-2; row>=0;row--){
+      for (let col = n-2;col>=0;col--){
+        dp[col] = dp[col]+dp[col+1]
+      }
+    }
+    return dp[0]
+    }
+}
