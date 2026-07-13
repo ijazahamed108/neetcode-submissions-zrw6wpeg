9e89836974ea3ed9cb7323c3aecc816d0e219d7b
Ijaz Ahamed <41547944+ijazahamed108@users.noreply.github.com>
2026-07-14 00:27:45 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        let jumps = 0;
+        let currentEnd=0;
+        let farthest=0;
+
+        for (let i=0;i<nums.length-1;i++){
+            farthest = Math.max(farthest, i+nums[i]);
+            if(i === currentEnd){
+                jumps++;
+                currentEnd = farthest;
+                if(currentEnd >= nums.length-1){
+                    break;
+                }
+            }
+
+        }
+        return jumps;
+    }
+}
