3b1a61e8a98a73ee783006110ae3fa15fffc4800
Ijaz Ahamed <41547944+ijazahamed108@users.noreply.github.com>
2026-07-19 22:42:12 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.js
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        if (intervals.length <= 1) return true;
+
+        intervals.sort((a, b) => a.start - b.start);
+
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < intervals[i - 1].end) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
